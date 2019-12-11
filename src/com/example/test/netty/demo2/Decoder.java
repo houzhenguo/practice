b932ae52eabea093b82876de7e92b9c24ec9cd8b
houzhenguo <houzhenguo@pwrd.com>
2019-12-11 17:12:16 +0800

    decode的部分提交

--- src/com/example/test/netty/demo2/Decoder.java
+++ src/com/example/test/netty/demo2/Decoder.java
@@ -16,12 +16,28 @@ public class Decoder extends ReplayingDecoder<Void>{
     int count;
     @Override
     protected void decode(ChannelHandlerContext ctx, ByteBuf in, List<Object> out) throws Exception {
+
         System.out.println("decode invoke count "+(++count));
+
+
         byte type = in.readByte();
         byte serial = in.readByte();
+
         in.markReaderIndex();
-        int size = in.readByte(); // size -> int
-        byte[] data = new byte[size+1]; // 这里是加上 bean 的长度那个字节
+        // 处理头部被压缩的问题
+        byte[] headdata = new byte[4]; // 这里需要的修改，不能写死。 这个可以放在外面 4个字节为最大长度
+        in.readBytes(headdata);
+        in.resetReaderIndex();
+        OctetsStream os = new OctetsStream();
+        os.wraps(headdata);
+        int posBegin = os.position();
+        int size = os.unmarshalUInt(); // 解析出头部长度
+        int posEnd = os.position();
+
+
+        in.markReaderIndex();
+        //int size = in.readByte(); // size -> int
+        byte[] data = new byte[size + posEnd  - posBegin]; // 这里是加上 bean 的长度那个字节
         in.resetReaderIndex();
         in.readBytes(data);
 
